14558ec6c8106cbe706a6df85f0d62e401a70c4a
CodeRaiden <hadoken10@yahoo.com>
2022-06-07 14:00:13 -0700

    added 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "_putchar.h"
+
+/**
+ *main - Entry point
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+        char c[] = "Holberton";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
+
