e0e621c4c7e3e8957a25535c468f6c458f35967a
CodeRaiden <hadoken10@yahoo.com>
2022-06-23 15:27:00 -0700

    add 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,21 +1,15 @@
 #include "main.h"
-
 /**
-  * _print_rev_recursion - Print a string in reverse
-  * @s: the string to reverse
-  *
-  * Return: Nothing.
-  */
+ * _print_rev_recursion - print a string in reverse
+ * @s: char pointer
+ * Return: void.
+ */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		return;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-
-	s++;
-	_print_rev_recursion(s);
-	s--;
-	putchar(*s);
 }
 
