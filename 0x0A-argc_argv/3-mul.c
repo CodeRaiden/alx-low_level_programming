36c7b7fd4ca38442e3d6cd729cfbf689d23bb00d
CodeRaiden <hadoken10@yahoo.com>
2022-06-25 18:32:58 -0700

    fix 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,18 +12,19 @@
 
 int main(int argc, char *argv[])
 {
-	int num1 = 0; num2 = 0; result = 0;
+	int n1 = 0, n2 = 0;
 
-	if(argc != 3)
+	if (argc == 3)
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%d\n", n1 * n2);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	result = num1 * num2;
-	printf("%d\n", result);
 
 	return (0);
 }
