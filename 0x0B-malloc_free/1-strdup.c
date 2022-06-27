443a9223bd5f7443495aaa27ebe305754139c6f5
CodeRaiden <hadoken10@yahoo.com>
2022-06-27 13:35:31 -0700

    add 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  * _strdup - Duplicate a string
+  * @str: the string to duplicate
+  *
+  * Return: the string duplicated
+  */
+
+char *_strdup(char *str)
+{
+	int i = 1, int a = 0;
+
+	if(str == NULL)
+	{
+		return (NULL);
+	}
+
+	char *s = malloc(i * sizeof(char) + 1);
+
+	while(str[i])
+	{
+		i++;
+	}
+
+	if(str == NULL)
+        {
+                return (NULL);
+        }
+
+	while(a < i)
+	{
+		s[a] = str[a];
+	}
+
+	s[a] = '\0';
+	return (s);
+}
+
+
