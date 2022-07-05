0e66d0198e3e478673c1286c02c4d4731c35971d
CodeRaiden <hadoken10@yahoo.com>
2022-07-05 05:52:59 -0700

    add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+/**
+ * struct dog - dog information.
+ *
+ * @name: Pointer to dog name.
+ * @age: Variable for age of dog.
+ * @owner: Pointer owner.
+ *
+ * Description: Information about a dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+
