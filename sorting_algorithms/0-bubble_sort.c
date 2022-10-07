b806864ac12f3a9aa5cfcf973b77e6b9100856be
CodeRaiden <hadoken10@yahoo.com>
2022-10-07 06:07:56 -0700

    first commit

--- sorting_algorithms/0-bubble_sort.c
+++ sorting_algorithms/0-bubble_sort.c
@@ -1,31 +1,37 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sort an array in ascending order
- * @array: the start of the array
- * @size: the size of the array
+ * bubble_sort - sorts an array of integers in ascending order using the
+ * Bubble sort algorithm
+ * @array: The array to be sorted
+ * @size: The size of the array
+ *
+ * Return: void
  */
 void bubble_sort(int *array, size_t size)
 {
-	int *position = NULL;
-	int status = 0;
+	size_t i, n, new_n;
+	int swap;
 
-	if (array && size > 1)
+	if (array == NULL || size < 2)
+		return;
+
+	n = size;
+	while (n > 0)
 	{
-		do {
-			position = array;
-			status = 0;
-			do {
-				if (position[1] < position[0])
-				{
-					position[0] ^= position[1];
-					position[1] ^= position[0];
-					position[0] ^= position[1];
-					status = 1;
-					print_array(array, size);
-				}
-			} while (++position + 1 < array + size);
-		} while (status);
+		new_n = 0;
+		for (i = 0; i < n - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = swap;
+				new_n = i + 1;
+				print_array(array, size);
+			}
+		}
+		n = new_n;
 	}
 }
 
