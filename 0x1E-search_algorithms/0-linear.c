fc9ad4651e8f7b918da914ba8c77ae60770f7b0a
CodeRaiden <hadoken10@yahoo.com>
2023-01-03 08:00:34 -0800

    add function that searches for a value in an array of integers using the Linear search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,15 +18,15 @@ int linear_search(int *array, size_t size, int value)
 
 	if (array)
 	{
-		for (i = 0; i < size; ++i)
+		for (i = 0; i < size; i++)
 		{
 			VALUE_CHECKED(array, i);
 			if (array[i] == value)
 			{
-				return i;
+				return (i);
 			}
 		}
 	}
-	return -1;
+	return (-1);
 }
 
